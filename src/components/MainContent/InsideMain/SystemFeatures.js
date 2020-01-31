1e78a2e755b9d07798fda1aa145f4b9c1d3b1b74
swikritii <58209841+swikritii@users.noreply.github.com>
2020-01-31 16:04:41 +0545

    home page -> feature section: bg image problem solved

--- src/components/MainContent/InsideMain/SystemFeatures.js
+++ src/components/MainContent/InsideMain/SystemFeatures.js
@@ -1,25 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Groupbg from '../../../img/Group149.png';
 
 function SystemFeatures() {
     return (
         <section className="features pdt-110 pdb-110">
-            <div className="container-fluid">
-                <div className="row-wrap">
-                    <div className="content-wrap">
-                        <span className="section-heading">
-                            System Features
-                        </span>
-                        <h3 className="heading3">This system is primarily designed to visualize the <span> household status of families living.</span></h3>
-                        <ul className="body2">
-                            <li><i className="material-icons">check</i>System login and roles</li>
-                            <li><i className="material-icons">check</i>Household survey data entry/upload</li>
-                            <li><i className="material-icons">check</i>Filter for data visualization</li>
-                            <li><i className="material-icons">check</i>Map based visualization</li>
-                            <li><i className="material-icons">check</i>Overlay Map layers for disasters</li>
-                            <li><i className="material-icons">check</i>Graphs and charts for better visualization</li>
-                            <li><i className="material-icons">check</i>Export household data</li>
-                            <li><i className="material-icons">check</i>Single household data view in a separate page</li>
-                        </ul>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <figure>
+                            <img src={Groupbg} alt='' />
+                        </figure>
+                        <div className="col-md-7">
+                            <div className="content-wrap">
+                                <span className="section-heading">
+                                    System Features
+                                </span>
+                                <h3 className="heading3">This system is primarily designed to visualize the <span> household status of families living.</span></h3>
+                                <ul className="body2">
+                                    <li><i className="material-icons">check</i>System login and roles</li>
+                                    <li><i className="material-icons">check</i>Household survey data entry/upload</li>
+                                    <li><i className="material-icons">check</i>Filter for data visualization</li>
+                                    <li><i className="material-icons">check</i>Map based visualization</li>
+                                    <li><i className="material-icons">check</i>Overlay Map layers for disasters</li>
+                                    <li><i className="material-icons">check</i>Graphs and charts for better visualization</li>
+                                    <li><i className="material-icons">check</i>Export household data</li>
+                                    <li><i className="material-icons">check</i>Single household data view in a separate page</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
