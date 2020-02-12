6a4b583dd7e9d0b943a282781f5179516af209e0
royallsilwallz <royalsilwal@gmail.com>
2020-02-12 14:28:04 +0545

    create measurement icon fix

--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             i: 0
-        }
-    }
-    
-    render() {
-        return (
-            <div className="card-header">
-            <a href="index.html" className="logo">
-                {/* <img src="img/logo.png" alt="logo" /> */}
-            </a>
-            <ul className="sidebar-nav" style={{listStyleType:'none'}}>
-                <li className={this.state.i===0 ? "current" : ""} onClick={() => this.setState({...this.state,i:0})}><Link to="/home">survey</Link></li>
-                <li className={this.state.i!==0 ? "current" : ""} onClick={() => this.setState({...this.state,i:1})}><Link to="/about">about</Link></li>
-            </ul>
-        </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      i: 0
+    };
+  }
+
+  render() {
+    return (
+      <div className='card-header'>
+        <a href='index.html' className='logo'>
+          {/* <img src="img/logo.png" alt="logo" /> */}
+        </a>
+        <ul className='sidebar-nav' style={{ listStyleType: 'none' }}>
+          <li
+            className={this.state.i === 0 ? 'current' : ''}
+            onClick={() => this.setState({ ...this.state, i: 0 })}
+          >
+            <Link to='/home'>survey</Link>
+          </li>
+          <li
+            className={this.state.i !== 0 ? 'current' : ''}
+            onClick={() => this.setState({ ...this.state, i: 1 })}
+          >
+            <Link to='/about'>about</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 export default Header;
